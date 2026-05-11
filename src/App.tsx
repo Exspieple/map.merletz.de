c006d6d8d5620cd9016ad4a80f68393e32597485
Exspieple <xx99593@gmail.com>
2026-05-11 13:23:31 +0200

    First map commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css'
+import "./App.css";
+
+import {
+  Map,
+  MapMarker,
+  MapPopup,
+  MapTileLayer,
+  MapZoomControl,
+} from "./components/ui/map";
 
 function App() {
   return (
     <>
-      Hello world!
+      <div className="h-full">
+        <Map center={[43.6532, -79.3832]} className="h-full">
+          <MapTileLayer />
+          <MapZoomControl />
+          <MapMarker position={[43.6532, -79.3832]}>
+            <MapPopup>A map component for shadcn/ui.</MapPopup>
+          </MapMarker>
+        </Map>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
